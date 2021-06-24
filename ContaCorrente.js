2a9e49df62eb9b157319575d32c4a4ae2c1102b6
Luiz Otavio Amboni <58530274+luiz-amboni@users.noreply.github.com>
2021-06-23 22:44:01 -0300

    Modularizando o código

---
+++ ContaCorrente.js
@@ -0,0 +1,28 @@
+export class ContaCorrente{
+    agencia
+    cliente;
+
+    // #saldo = 0  https://github.com/tc39/proposal-class-fields#private-fields
+    _saldo = 0
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0){
+            return;
+        }
+
+        this._saldo += valor
+    }
+
+    transferir(valor, conta){
+        conta.cidade = "São Paulo"
+        const valorScado = this.sacar(valor)
+        conta.depositar(valorScado)
+    }
+}
--- index.js
+++ index.js
@@ -1,28 +1,5 @@
-class Cliente{
-    nome
-    cpf
-}
-
-class ContaCorrente{
-    agencia
-    // #saldo = 0  https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo = 0
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0){
-            return;
-        }
-
-        this._saldo += valor
-    }
-}
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente()
 cliente1.nome = "Ricardo"
@@ -34,7 +11,17 @@ cliente2.cpf = 88822233309
 
 const contaCorrenteRicardo = new ContaCorrente()
 contaCorrenteRicardo.agencia = 1001
-contaCorrenteRicardo.depositar(100)
-const valorSacado = contaCorrenteRicardo.sacar(50)
-console.log(valorSacado)
+contaCorrenteRicardo.cliente = cliente1
+contaCorrenteRicardo.depositar(500)
+
+const conta2 = new ContaCorrente()
+conta2.cliente = cliente2
+conta2.agencia = 2
+
+console.log(conta2)
+console.log(contaCorrenteRicardo)
+
+contaCorrenteRicardo.transferir(200, conta2)
+
+console.log(conta2)
 console.log(contaCorrenteRicardo)
