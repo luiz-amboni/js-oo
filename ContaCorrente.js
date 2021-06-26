28278bfdbb2a19c67282c7a2f1dffe3b61bff52b
Luiz Otavio Amboni <58530274+luiz-amboni@users.noreply.github.com>
2021-06-26 00:41:48 -0300

    Implementando classe Conta e ajustando conta corrente

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -2,12 +2,8 @@ import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente{
     static numeroDeContas = 0;
-    agencia;
-    _cliente;
    // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-   _saldo = 0;
     
-
 
     set cliente(novoValor){
         if(novoValor instanceof Cliente){
@@ -26,6 +22,7 @@ export class ContaCorrente{
     constructor(agencia, cliente){
         this.agencia = agencia;
         this.cliente = cliente;
+        this._saldo = 0;
         ContaCorrente.numeroDeContas += 1;
     }
 
@@ -38,7 +35,7 @@ export class ContaCorrente{
     }
 
     depositar(valor){
-        if(valor <= 0)
+        if(valor <= 100)
         {
             return;
         } 
